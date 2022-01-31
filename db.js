870f1b235af965d1e7faa3167f9e4f7262fc67bf
chris097 <chrisfidel.international@gmail.com>
2022-01-31 17:39:13 +0100

    connect database to heroku

--- db.js
+++ db.js
@@ -1,12 +1,11 @@
 const Pool = require('pg').Pool;
 
 const pool = new Pool({
-  user: "postgres",
-  password: "chris@1995",
-  host: "localhost",
+  user: "vrkiibwqjatdls",
+  host: "ec2-3-217-216-13.compute-1.amazonaws.com",
   port: 5432,
-  database: "bookshelter"
+  password: 'a5e1c90dc03bd35c91f2a22a61c1079f7800dae71f590af726e62d96b9d2f9aa',
+  database: "dbu664sisfdv9i"
 });
 
 module.exports = pool;
-
